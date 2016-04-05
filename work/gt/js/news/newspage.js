317505a255959b4e834181d228415152efec9099
infi000 <113079767@qq.com>
2016-04-05 16:41:52 +0800

    gt

--- work/gt/js/news/newspage.js
+++ work/gt/js/news/newspage.js
@@ -3,6 +3,7 @@ var reduceFontsize = $(".reduceFontsize");
 var resetFontsize = $(".resetFontsize");
 var h1 = $(".content").find("h1");
 var p = $(".content").find("p");
+p.css({ "font-size":"20px" });
 // addFontsize.on("click", add_FontSize(p, h1));
 addFontsize.on("click", function() {
     add_FontSize(p, h1)
